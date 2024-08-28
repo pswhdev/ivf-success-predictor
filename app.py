68ff61ea2edcc385cc4b95725a15161ca06e7f49
camimmi <patriciaswhalley@gmail.com>
2024-08-28 19:18:48 +0200

    Add Dashboard Desing to README

--- app.py
+++ app.py
@@ -3,10 +3,15 @@ from app_pages.multipage import MultiPage
 
 # load pages scripts
 from app_pages.page_summary import page_summary_body
+from app_pages.page_eda_ivf_treatment import page_eda_ivf_treatment_body
 
 # Create an instance of the app
-app = MultiPage(app_name= "IVF Predictor")
+app = MultiPage(app_name="IVF Predictor")
 
 app.add_page("Quick Project Summary", page_summary_body)
+app.add_page(
+    "Exploratory Analysis of IVF Treatment Data",
+    page_eda_ivf_treatment_body
+    )
 
 app.run()
