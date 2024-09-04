9db7b2fb8627b069af3a2d32c55d4dfc821b51d4
camimmi <patriciaswhalley@gmail.com>
2024-09-04 20:36:09 +0200

    Improve explanation page_ml_success_predictor

--- app_pages/page_ml_success_predictor.py
+++ app_pages/page_ml_success_predictor.py
@@ -5,7 +5,7 @@ from src.data_management import (
     load_ifv_treatment_data,
     load_pkl_file,
     load_gzip_file
-    )
+)
 from src.machine_learning.evaluate_clf import clf_performance
 
 
@@ -13,27 +13,38 @@ def page_ml_success_predictor_body():
 
     version = "v1"
     # load needed files
-    ivf_pipe_dc_fe = load_pkl_file(
-        f"outputs/ml_pipeline/ivf_success_predictor/{version}/"
-        "clf_pipeline_data_cleaning_feat_eng.pkl"
+    ivf_pipe_dc = load_pkl_file(
+        f"outputs/ivf_success_predictor/data_cleaning_pipeline/{version}/"
+        "data_cleaning_pipeline.pkl"
     )
+
+    ivf_preprocessing = load_pkl_file(
+        f"outputs/ml_pipeline/ivf_success_predictor/{version}/"
+        "clf_pipeline_pre_processing.pkl"
+    )
+
     ivf_pipe_model = load_gzip_file(
         f"outputs/ml_pipeline/ivf_success_predictor/{version}/"
         "clf_pipeline_model.pkl.gz"
     )
+
     ivf_feat_importance = plt.imread(
         f"outputs/ml_pipeline/ivf_success_predictor/{version}"
         "/features_importance.png"
     )
+
     X_train = pd.read_csv(
         f"outputs/ml_pipeline/ivf_success_predictor/{version}/X_train.csv"
     )
+
     X_test = pd.read_csv(
         f"outputs/ml_pipeline/ivf_success_predictor/{version}/X_test.csv"
     )
+
     y_train = pd.read_csv(
         f"outputs/ml_pipeline/ivf_success_predictor/{version}/y_train.csv"
     ).values
+
     y_test = pd.read_csv(
         f"outputs/ml_pipeline/ivf_success_predictor/{version}/y_test.csv"
     ).values
@@ -42,29 +53,42 @@ def page_ml_success_predictor_body():
     # display pipeline training summary conclusions
     st.info(
         """
-        * The pipeline was tuned aiming at least 0.70 Precision on F1 Score
-        for Success, since we are interested in this project in predicting
-        chances of IVF Treatment success.
+        * The pipeline was tuned to achieve a mean F1 score of at least 0.7.
+        This is crucial because the goal is to minimize the risk of
+        providing false hope by predicting treatment success when there
+        is a high chance of failure.
 
-        * The pipeline performance on train and test set is 0.79 and 0.49,
-        respectively.
+        * However, due to the complexity of biological interactions that
+        are not fully captured by the dataset, and the low predictive
+        power of the variables as indicated by correlation analysis, the ML
+        model did not achieve the desired predictive performance. The model's
+        mean F1 Score is 0.74 on the training set but drops significantly to
+        0.57 on the test set.
         """
     )
 
     # show pipelines
     st.write("---")
-    st.write("#### There are 2 ML Pipelines arranged in series.")
+    st.write("#### There are 3 ML Pipelines:")
 
     st.write(
         """
-        * The first is responsible for data cleaning and feature engineering.
+        The first is responsible for data cleaning and it is shared by all ML
+        Models.
         """
     )
-    st.write(ivf_pipe_dc_fe)
+    st.write(ivf_pipe_dc)
 
     st.write(
         """
-        * The second is for feature scaling and modelling.
+        The second is carries on further feature engineering tasks.
+        """
+    )
+    st.write(ivf_preprocessing)
+
+    st.write(
+        """
+        The third one is the modelling pipeline.
         """
     )
     st.write(ivf_pipe_model)
@@ -76,6 +100,7 @@ def page_ml_success_predictor_body():
         The features the model was trained and their importance.
         """
     )
+
     st.write(X_train.columns.to_list())
     st.image(ivf_feat_importance)
 
